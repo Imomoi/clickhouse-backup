7a3abae041cd45e1fd62c7646a709bbbe2a4f8d4
Viacheslav Molokov <viacheslav.molokov@gmail.com>
2020-10-02 11:49:01 +0300

    master. shhh

--- examples/crontask.sh
+++ examples/crontask.sh
@@ -2,5 +2,6 @@
 
 source /usr/local/rvm/environments/default
 cd /usr/local/infrastructure.clickhouse_backup/
+bundle update --bundler
 bundle install
 nice -n 10 ruby /usr/local/infrastructure.clickhouse_backup/bin/clickhouse_backup /usr/local/infrastructure.clickhouse_backup/config.yml
